929bd5804f678ae2063ce985e6cb2adf71d97b76
Christian Pekeler <christian@pekeler.org>
2026-02-04 15:19:31 +0100

    Add pocketbun binary wrapper

--- index.ts
+++ index.ts
@@ -6,5 +6,5 @@ export { type SuperuserOtpResult } from "./src/cmd/superuser.ts";
 export { BaseApp, type BaseAppConfig } from "./src/core/base.ts";
 export { type App } from "./src/core/app.ts";
 export { migrate, type MigrateMode } from "./src/core/migrate.ts";
-export { PocketBase, type PocketBaseConfig } from "./src/pocketbase.ts";
+export { New, NewWithConfig, PocketBase, type PocketBaseConfig, Version } from "./src/pocketbase.ts";
 export { DbxDatabase, attachDbxRewrite, rewriteDbxIdentifiers } from "./src/tools/dbx/index.ts";
